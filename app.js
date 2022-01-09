3382659db6b6af87d3bcbd1b73b271a866877f8b
ne0rad <usackasr@gmail.com>
2022-01-09 20:50:33 +0000

    Updated socket.io to have rooms and users

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 
 const server = http.createServer(app);
 
+const { createUser, removeUser, getUser } = require("./user");
+
 const io = socketIo(server, {
     cors: {
         origin: "http://localhost:3000",
@@ -16,16 +18,32 @@ const io = socketIo(server, {
 
 io.on("connection", (socket) => {
     let time = new Date().toLocaleTimeString();
-
     console.log('\x1b[32m%s\x1b[0m', `[socket.io][${time}] Client connected.`);
+
+    socket.on('connect', ({ username, roomID }, callback) => {
+        if (!username || !roomID) return callback("Username and RoomID can't be empty.");
+        const user = createUser(username, roomID, socket.id);
+        if (user.error) return callback(user.error);
+
+        socket.join(roomID);
+        socket.in(roomID).emit('notification', `${username} joined the room.`);
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        let user = getUser(socket.id);
+        if (user.error) return callback(user.error);
+        socket.in(user.roomID).emit('message', message);
+        callback();
+    })
 
     socket.on("disconnect", () => {
         console.log('\x1b[31m%s\x1b[0m', `[socket.io][${time}] Client disconnected.`);
-    });
-
-    socket.on('chat', (message) => {
-        let date = new Date();
-        io.emit('chat', {date: date, message: message});
+        let user = getUser(socket.id);
+        if (!user.error && user) {
+            socket.in(user.roomID).emit('notification', `${user.username} left the room.`);
+            removeUser(socket.id);
+        }
     });
 });
 
