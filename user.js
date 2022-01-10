c2abade1de55ff301b9f07707724f41b8a0ea470
ne0rad <usackasr@gmail.com>
2022-01-10 18:33:39 +0000

    Check if user already exists

--- user.js
+++ user.js
@@ -1,8 +1,16 @@
 const users = {};
 
 exports.createUser = function (username, roomID, socketID) {
-    let user = users[socketID];
-    if (user) {
+    username = username.trim();
+    roomID = roomID.trim();
+    let userExists = false;
+    for (const [key, value] of Object.entries(users)) {
+        if (value.username.toLowerCase() == username.toLowerCase()) {
+            userExists = true;
+            break;
+        }
+    }
+    if (userExists) {
         return { error: "Username already taken." }
     } else {
         users[socketID] = { username: username, roomID: roomID }
